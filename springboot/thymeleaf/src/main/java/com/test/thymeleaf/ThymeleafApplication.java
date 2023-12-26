62afba49cd8528736916cbea8957a663a76c5a5f
eeunha <eunha.dev@gmail.com>
2023-12-26 11:55:16 +0900

    231226 수업

--- springboot/thymeleaf/src/main/java/com/test/thymeleaf/ThymeleafApplication.java
+++ springboot/thymeleaf/src/main/java/com/test/thymeleaf/ThymeleafApplication.java
@@ -1,9 +1,11 @@
 package com.test.thymeleaf;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.test.thymeleaf.mapper") // 해당 패키지 내의 mapper 쿼리 사용
 public class ThymeleafApplication {
 
 	public static void main(String[] args) {
